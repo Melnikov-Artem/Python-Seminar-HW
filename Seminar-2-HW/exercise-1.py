9345946220b928cd81d732251feed467b203c443
Artem <artem.melnikov.ara@gmail.com>
2022-10-06 23:22:50 +0300

    Seminar 2 HW Exercise 1

--- Seminar-2-HW/exercise-1.py
+++ Seminar-2-HW/exercise-1.py
@@ -1,23 +1,17 @@
-# ; 1.	Напишите программу, которая принимает на вход вещественное число
-# ; и показывает сумму его цифр.
-# ; Пример:
-# ; o	6782 -> 23
-# ; o	0,56 -> 11
+# 1.	Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
 
 number = float(input('Введите вещественное число: '))
 res = 0
-# print(0.56/2)
-# print(0.56 % 1)
-# print(0.56//1)
 while (number//1) != 0:
     res = res + number % 10
     number = number//10
-    print(f'number={number}')
-    print(f'res={res}')
 if (number % 1) != 0:
     ostatok=str(number)
     print(ostatok)
     for i in range (2,len(ostatok)):
-        print(f'ostatok={ostatok[i]}')
         res = res+int(ostatok[i])
-print(f'res={res}')
+print(f'Сумма чисел = {int(res)}')
